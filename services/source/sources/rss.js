cae1dc7564e837531d667b93b73f02c95c484367
Eirik Vullum <evullum@gmail.com>
2014-08-17 13:56:33 +0200

    add thewirecutter

--- services/source/sources/rss.js
+++ services/source/sources/rss.js
@@ -2,6 +2,67 @@ exports = module.exports = [
     {
         active: true,
         origin: 'feed',
+        name: 'TheWireCutter',
+        url: 'http://feeds.feedburner.com/TheWirecutter',
+        linkref: 'url',
+        category: ['technology' ,'thewirecutter'],
+        format: 'desktop',
+        body: true,
+        template: {
+            elements: [
+                {
+                    name: 'guid',
+                    type: 'url',
+                    required: true,
+                    items: [
+                        {
+                            selector: 'guid'
+                        },
+                        {
+                            selector: 'link'
+                        },
+                        {
+                            selector: 'title',
+                            decode: true
+                        }
+                    ]
+                },
+                {
+                    name: 'title',
+                    required: true,
+                    items: [
+                        {
+                            selector: 'title',
+                            decode: true
+                        }
+                    ]
+                },
+                {
+                    name: 'url',
+                    type: 'url',
+                    required: true,
+                    items: [
+                        {
+                            selector: 'link'
+                        }
+                    ]
+                },
+                {
+                    name: 'image',
+                    type: 'url',
+                    items: [
+                        {
+                            selector: 'enclosures[0].url'
+                        }
+                    ],
+                    fallback: 'http://thewirecutter.wpengine.netdna-cdn.com/wp-content/themes/thewirecutter/thewirecutter/img/logo/header.png'
+                }
+            ]
+        }
+    },
+    {
+        active: true,
+        origin: 'feed',
         name: 'Gamer.no',
         url: 'http://www.gamer.no/feeds/general.xml',
         linkref: 'url',
