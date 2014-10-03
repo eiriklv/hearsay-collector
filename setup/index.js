17ff5c7755ff693836919a48ddd0566bfa3c75b5
Eirik Vullum <evullum@gmail.com>
2014-10-03 10:21:53 +0200

    add mongoose error handling and automatic reconnect

--- setup/index.js
+++ setup/index.js
@@ -1,5 +1,46 @@
-var url = require('url');
+var debug = require('debug')('hearsay:collector:setup');
 
-module.exports.db = function (db, config){
-    db.connect(config.get('mongo_url'));
+module.exports.db = function (mongoose, config){
+    function connect() {
+        mongoose.connect(config.get('mongo_url'));
+    }
+
+    // connection is open and ready
+    mongoose.connection.on('open', function (ref) {
+        debug('open connection to mongo server.');
+    });
+
+    // mongoose is connected to server
+    mongoose.connection.on('connected', function (ref) {
+        debug('connected to mongo server.');
+    });
+
+    // mongoose has disconnected
+    mongoose.connection.on('disconnected', function (ref) {
+        debug('disconnected from mongo server.');
+
+        debug('retrying connection in 2 seconds..');
+        setTimeout(function() {
+            connect();
+        }.bind(this), 2000);
+    });
+
+    // mongoose connection has closed
+    mongoose.connection.on('close', function (ref) {
+        debug('closed connection to mongo server');
+    });
+
+    // error has occured for mongoose connection
+    mongoose.connection.on('error', function (err) {
+        debug('error connection to mongo server!');
+        debug(err);
+    });
+
+    // mongoose is reconnecting
+    mongoose.connection.on('reconnect', function (ref) {
+        debug('reconnect to mongo server.');
+    });
+
+    // initial connect
+    connect();
 };
