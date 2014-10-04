e83de6422ed032dd0e6afdba3697aced29699226
Eirik Vullum <evullum@gmail.com>
2014-10-04 10:45:18 +0200

    add mongoose error handling and automatic reconnect

--- setup/index.js
+++ setup/index.js
@@ -1,22 +1,22 @@
 var debug = require('debug')('hearsay:collector:setup');
 
-module.exports.db = function (mongoose, config){
+module.exports.db = function(mongoose, config) {
     function connect() {
         mongoose.connect(config.get('mongo_url'));
     }
 
     // connection is open and ready
-    mongoose.connection.on('open', function (ref) {
+    mongoose.connection.on('open', function(ref) {
         debug('open connection to mongo server.');
     });
 
     // mongoose is connected to server
-    mongoose.connection.on('connected', function (ref) {
+    mongoose.connection.on('connected', function(ref) {
         debug('connected to mongo server.');
     });
 
     // mongoose has disconnected
-    mongoose.connection.on('disconnected', function (ref) {
+    mongoose.connection.on('disconnected', function(ref) {
         debug('disconnected from mongo server.');
 
         debug('retrying connection in 2 seconds..');
@@ -26,18 +26,18 @@ module.exports.db = function (mongoose, config){
     });
 
     // mongoose connection has closed
-    mongoose.connection.on('close', function (ref) {
+    mongoose.connection.on('close', function(ref) {
         debug('closed connection to mongo server');
     });
 
     // error has occured for mongoose connection
-    mongoose.connection.on('error', function (err) {
+    mongoose.connection.on('error', function(err) {
         debug('error connection to mongo server!');
         debug(err);
     });
 
     // mongoose is reconnecting
-    mongoose.connection.on('reconnect', function (ref) {
+    mongoose.connection.on('reconnect', function(ref) {
         debug('reconnect to mongo server.');
     });
 
